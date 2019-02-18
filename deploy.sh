fec8018388b4d9987c333b35f89c8dddcdaf36d8
Yunchao Du <ycdu@thoughtworks.com>
2019-02-19 00:11:41 +0800

    update

--- deploy.sh
+++ deploy.sh
@@ -16,6 +16,7 @@ deployPort=$(randPort 10000 60000)
 clearCmd="/opt/deploy-service/clear-service.sh ${serviceName}
 loadCmd="/opt/deploy-service/docker-image.sh load ${serviceName} ${BUILD_NUMBER}"
 runCmd="docker run --env env=${env} --env deployIp=${deployIp} --env deployPort=${deployPort} -it -d -p ${deployPort}:${deployPort} --name ${serviceName} ${serviceName}:${BUILD_NUMBER}"
+
 if [ "local" = "${env}" ];then
     echo "deploy to local"
     ${clearCmd}
@@ -25,11 +26,11 @@ if [ "local" = "${env}" ];then
 fi
 
 src_dir=${images_path}${serviceName}/${serviceName}_${BUILD_NUMBER}.tar
-dest_dir=${images_path}
+dest_dir=${images_path}${serviceName
 if [ "uat" = "${env}" ];then
-    echo "scp docker image to remote target server"
+    echo "scp docker image to remote target server..."
     /opt/deploy-service/expect_scp.sh $uat_host $uat_port $uat_user $uat_pwd $src_dir $dest_dir
-    echo "run docker to target env server"
+    echo "run docker to target env server..."
 
     #yum install sshpass if not install sshpass at build server
     sshpass -p $uat_pwd ssh $uat_user@${uat_host} ${clearCmd}
