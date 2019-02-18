a241ed61a3171eae5824da08d459b11a967cd745
Yunchao Du <ycdu@thoughtworks.com>
2019-02-19 00:22:44 +0800

    update

--- scp.sh
+++ scp.sh
@@ -3,6 +3,6 @@
 serviceName=$1
 BUILD_NUMBER=$2
 src_dir=${images_path}${serviceName}/${serviceName}_${BUILD_NUMBER}.tar
-dest_dir=${images_path}${serviceName
+dest_dir=${images_path}${serviceName}
 echo "scp docker image to remote target server..."
 /opt/deploy-service/expect_scp.sh $uat_host $uat_port $uat_user $uat_pwd $src_dir $dest_dir
