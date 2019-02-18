aae38d4fd2066dcc1cedbbf25525bee6510c2d41
Caspar Du <44493650+Caspar7@users.noreply.github.com>
2019-02-19 00:54:26 +0800

    Update deploy.sh
    update deploy

--- deploy.sh
+++ deploy.sh
@@ -13,23 +13,6 @@ randPort(){
 
 echo "deploy docker container..."
 deployPort=$(randPort 10000 60000)
-clearCmd="/opt/deploy-service/clear-service.sh ${serviceName}
-loadCmd="/opt/deploy-service/docker-image.sh load ${serviceName} ${BUILD_NUMBER}"
-runCmd="docker run --env env=${env} --env deployIp=${deployIp} --env deployPort=${deployPort} -it -d -p ${deployPort}:${deployPort} --name ${serviceName} ${serviceName}:${BUILD_NUMBER}"
-
-if [ "local" = "${env}" ];then
-    echo "deploy to local"
-    ${clearCmd}
-    ${loadCmd}
-    docker run --env env=${env} --env deployIp=${deployIp} --env deployPort=${deployPort} -it -d -p ${deployPort}:${deployPort} --name ${serviceName} ${serviceName}:${BUILD_NUMBER}
-    exit 0
-fi
-
-# if [ "uat" = "${env}" ];then
-#     echo "run docker to target env server..."
-#     #yum install sshpass if not install sshpass at build server
-#     sshpass -p $uat_pwd ssh $uat_user@${uat_host} ${clearCmd}
-#     sshpass -p $uat_pwd ssh $uat_user@${uat_host} ${loadcmd}
-#     sshpass -p $uat_pwd ssh $uat_user@${uat_host} ${runcmd}
-#     exit 0
-# fi
+/opt/deploy-service/clear-service.sh ${serviceName}
+/opt/deploy-service/docker-image.sh load ${serviceName} ${BUILD_NUMBER}"
+docker run --env env=${env} --env deployIp=${deployIp} --env deployPort=${deployPort} -it -d -p ${deployPort}:${deployPort} --name ${serviceName} ${serviceName}:${BUILD_NUMBER}"
