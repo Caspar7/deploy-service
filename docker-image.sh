3f128232cfdc2c51c046e25e25d8161f73448632
Yunchao Du <ycdu@thoughtworks.com>
2019-02-18 23:33:27 +0800

    update

--- docker-image.sh
+++ docker-image.sh
@@ -1,14 +1,17 @@
 #!/usr/bin/env bash
-./config.sh
+. config.sh
 op=$1
 serviceName=$2
 BUILD_NUMBER=$3
 
 if [ "save" = "${op}" ];then
-    docker save -o ${images_path}${serviceName}_${BUILD_NUMBER}.tar ${serviceName}:${BUILD_NUMBER}
+    echo "save docker image to ${images_path}${serviceName}/${serviceName}_${BUILD_NUMBER}.tar"
+    mkdir -p ${images_path}${serviceName}/
+    docker save -o ${images_path}${serviceName}/${serviceName}_${BUILD_NUMBER}.tar ${serviceName}:${BUILD_NUMBER}
 fi
 
 if [ "load" = "${op}" ];then
-    docker load -i ${serviceName}_${BUILD_NUMBER}.tar
+    echo "start load docker image to ${serviceName}_${BUILD_NUMBER}.tar"
+    docker load -i ${images_path}${serviceName}/${serviceName}_${BUILD_NUMBER}.tar
 fi
 
